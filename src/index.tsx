098230ad69b0bcfbdb7db95f46195de11ff502da
rasim <rasimzes@gmail.com>
2022-04-21 09:33:30 +0500

    Добавлены пустые папки

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.querySelector('#root');
+
 if (!rootElement) throw new Error('Failed to find the root element');
+
 const root = createRoot(rootElement);
 
 root.render(<App />);
